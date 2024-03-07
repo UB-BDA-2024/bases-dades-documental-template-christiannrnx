16d0a5b9466c775661187fb5b94c2b24f144e3b4
Christian Romero <christianrne@gmail.com>
2024-03-07 18:37:12 +0100

    ping create and delete 6 tests passed

--- app/sensors/repository.py
+++ app/sensors/repository.py
@@ -22,10 +22,42 @@ def create_sensor(db: Session, sensor: schemas.SensorCreate) -> models.Sensor:
 
 def record_data(redis: Session, sensor_id: int, data: schemas.SensorData) -> schemas.Sensor:
     db_sensordata = data
+
+    # Get and group dynamic data
+    dynamic_data = {
+        "temperature": data.temperature,
+        "humidity": data.humidity,
+        "battery_level": data.battery_level,
+        "last_seen": data.last_seen,
+    }
+
+    # Create key
+    key = f"sensor:{sensor_id}:data"
+    # Set dynamic data (with JSON format) to key in the redis db
+    redis.set(key, json.dumps(dynamic_data))
+
     return db_sensordata
 
 def get_data(redis: Session, sensor_id: int, data: schemas.SensorData) -> schemas.Sensor:
-    db_sensordata = data
+    
+    # Get sensor by id
+    db_sensor = db.query(models.Sensor).filter(models.Sensor.id == sensor_id).first()
+
+    # Create key
+    key = f"sensor:{sensor_id}:data"
+    # Get dynamic data assigned to key
+    dynamic_data = json.loads(redis.get(key))
+
+    # Group static and dynamic data
+    db_sensordata = {
+        "id": sensor_id,
+        "name": db_sensor.name,
+        "temperature": dynamic_data['temperature'],
+        "humidity": dynamic_data['humidity'],
+        "battery_level": dynamic_data['battery_level'],
+        "last_seen": dynamic_data['last_seen']
+    }
+
     return db_sensordata
 
 def delete_sensor(db: Session, sensor_id: int):
